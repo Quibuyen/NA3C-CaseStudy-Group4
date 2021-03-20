77a3fe85ab84290c70a592ef7cc1392ff1fd85cd
RalphLorenzPangilinan <79499882+RalphLorenzPangilinan@users.noreply.github.com>
2021-03-20 14:56:27 +0800

    Pangilinan_RalphLorenz_method_5&6

--- CaseStudy_5&6.java
+++ CaseStudy_5&6.java
@@ -9,14 +9,14 @@ public class CaseStudy {
 
 
         Scanner prog = new Scanner (System.in);
-         System.out.print("Car Model: ");
-        String model = prog.nextLine();
-        p.getModel(model);
-
-
+        
         System.out.print("Phone No.: ");
         int phoneNum= prog.nextInt();
         p.getPhoneNum(0);
+        
+        System.out.print("No. of hours To Rent: ");
+        int numD = prog.nextInt();
+        p.getNumofD(numD);
    }
 
  }
