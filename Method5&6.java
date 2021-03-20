b18c625f103495374c77849eef1bf130206c0270
RalphLorenzPangilinan <79499882+RalphLorenzPangilinan@users.noreply.github.com>
2021-03-20 14:58:36 +0800

    Pangilinan_RalphLorenz_method_5&6

--- Method5&6.java
+++ Method5&6.java
@@ -1,39 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package casestudy4.pkg5;
-
-/**
- *
- * @author Lorenz
- */
-import java.util.*;
-public class CaseStudy45 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-         Method m = new Method();
-
-
-        Scanner prog = new Scanner (System.in);
-        
-
-
-        System.out.print("Car Color: ");
-        String color = prog.nextLine();
-        m.getcolor(color);
-
-
-        System.out.print("Car Id No: ");
-        int ID= prog.nextInt();
-        m.getId(0);
-
-
-      
+public class Person 
+{
+     private int phoneNum;
+    private int numD;
+    
+    //Method 5: 
+    public void getPhoneNum(int phoneNum)
+    {
+        this.phoneNum = phoneNum;
     }
-
- }
+    
+    public int showPhoneNum()
+    {
+        return phoneNum;
+    }
+    
+    //Method 6: 
+    public void getNumofD(int numD)
+    {
+        this.numD = numD;
+    }
+    
+    public int showNumofD()
+    {
+        return numD;
+    }
+}
