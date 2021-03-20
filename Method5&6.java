40b54ef9d0ba94c83f56b6bf5f20781c636d6a3c
RalphLorenzPangilinan <79499882+RalphLorenzPangilinan@users.noreply.github.com>
2021-03-20 14:24:00 +0800

    Pangilinan_RalphLorenz_Method_5&6

---
+++ Method5&6.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casestudy4.pkg5;
+
+/**
+ *
+ * @author Lorenz
+ */
+class Method {
+    
+
+  
+    private String color;
+    private int Id;
+    
+
+ 
+
+    
+    
+    //Method 5: 
+    public void getcolor(String color)
+    {
+        this.color = color;
+    }
+    
+    public String showWork()
+    {
+        return color;
+    }
+    
+//Method 6: 
+    public void getId(int Id)
+    {
+        this.Id = Id;
+    }
+    
+    public int showId()
+    {
+        return Id;
+    }
+
+
+    
+}
